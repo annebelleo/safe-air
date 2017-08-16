226bfa2297e1628a49b43c763ee62e670a0a55a0
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-08-16 17:58:01 +0000

    tfw you realize you put ur main content in the head HA

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-var APIKey = "3v9tLnFRvW7F9bWGB";
-var userInput = document.getElementById('input').value;
-var endpoint = `http://api.airvisual.com//v2/city?city=los-angeles&key=${APIKey}`;
+// https://airvisual.com/api/documentation
 
-function results() {
-    fetch(endpoint)
-        .then(
-            function(data) {
-                return data.json();
-            })
+// var APIKey = "3v9tLnFRvW7F9bWGB";
+// var userInput = document.getElementById('input').value;
+// var endpoint = `http://api.airvisual.com//v2/nearest_city?lat=${latitude}&lon=${longitude}&rad=${radius}&key=${APIKey}`;
+var x = document.getElementById("demo");
+console.log(x);
 
-        .then(function(json) {
-            console.log(json);
-        })
-        .catch(
-            err => {
-                console.log(err);
-            });
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+// function results() {
+//     fetch(endpoint)
+//         .then(
+//             function(data) {
+//                 return data.json();
+//             })
+
+//         .then(function(json) {
+//             console.log(json);
+//         })
+//         .catch(
+//             err => {
+//                 console.log(err);
+//             });
+// }
