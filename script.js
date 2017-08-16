2e9224c4a36be19e0846d241fe46d731db9a1387
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-08-16 21:15:49 +0000

    hey the script tag is ON THE BOTTOM NOW

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 // https://airvisual.com/api/documentation
 
 var APIKey = "3v9tLnFRvW7F9bWGB";
-// var userInput = document.getElementById('input').value;
-
-var x = document.createElement("p");
+var x = document.getElementById("demo");
 var latitude;
 var longitude;
 
 function getLocation() {
-    console.log("function ran")
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.watchPosition(showPosition);
     }
     else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function showPosition(position) {
-    console.log("function ran");
-    latitude = position.coords.latitude
-    longitude = position.coords.longitude
-    return latitude;
-    return longitude;
+function showPosition(position, APIKey) {
     x.innerHTML = "Latitude: " + position.coords.latitude +
         "<br>Longitude: " + position.coords.longitude;
-    // console.log(x.innerHTML)
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    results(latitude, longitude, APIKey);
+    console.log("function ran");
 }
 
-var endpoint = `http://api.airvisual.com//v2/nearest_city?lat=${latitude}&lon=${longitude}&rad=5&key=${APIKey}`;
-console.log(endpoint);
+function getAPIData(latitude, longitude, APIKey) {
+    console.log("function ran")
+    results();
+}
 
-function results() {
+function results(latitude, longitude, APIKey) {
+    var endpoint = `http://api.airvisual.com//v2/nearest_city?lat=${latitude}&lon=${longitude}&rad=5&key=${APIKey}`;
     fetch(endpoint)
         .then(
             function(data) {
@@ -45,4 +43,21 @@ function results() {
             err => {
                 console.log(err);
             });
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred.";
+            break;
+    }
 }
