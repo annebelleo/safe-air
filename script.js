15994b5dbdaa3195f10090ad0e0e3e08f0eeaa7b
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-08-22 14:48:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // https://airvisual.com/api/documentation
 
 var APIKey = "3v9tLnFRvW7F9bWGB";
-var x = document.getElementById("demo");
+var x = document.getElementById("index");
+var y = document.getElementById("quality")
+var z = document.getElementById("results");
 var latitude;
 var longitude;
 
@@ -32,8 +34,46 @@ function results(latitude, longitude, APIKey) {
             console.log(json);
             var aqiIndex = json.data.current.pollution.aqius;
             x.innerHTML = `AQI Index: ${json.data.current.pollution.aqius}`;
+            var progress = document.createElement("div");
+            var bar = document.createElement("div");
+            progress.setAttribute("id", "myProgress");
+            bar.setAttribute("id", "myBar");
+            document.getElementById("progressBar").appendChild(progress);
+            progress.appendChild(bar);
+            move(aqiIndex);
             if (aqiIndex <= 50) {
-                console.log("things look good fam");
+                bar.style.backgroundColor = "green";
+                y.innerHTML = "Conditions: Good";
+                z.innerHTML = "Air quality is considered satisfactory, and air pollution poses little or no risk."
+            }
+            else if (aqiIndex <= 100) {
+                bar.style.backgroundColor = "yellow";
+                bar.style.color = "black";
+                y.innerHTML = "Conditions: Moderate";
+                z.innerHTML = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people. For example, people who are unusually sensitive to ozone may experience respiratory symptoms."
+            }
+            else if (aqiIndex <= 150) {
+                bar.style.backgroundColor = "orange";
+                y.innerHTML = "Conditions: Unhealthy for Sensitive Groups";
+                z.innerHTML = "Although general public is not likely to be affected at this AQI range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas persons with heart and lung disease, older adults and children are at greater risk from the presence of particles in the air."
+            }
+            else if (aqiIndex <= 200) {
+                bar.style.backgroundColor = "red";
+                y.innerHTML = "Conditions: Unhealthy";
+                z.innerHTML = "Everyone may begin to experience some adverse health effects, and members of the sensitive groups may experience more serious effects."
+            }
+            else if (aqiIndex <= 300) {
+                bar.style.backgroundColor = "purple";
+                y.innerHTML = "Conditions: Very Unhealthy";
+                z.innerHTML = "This would trigger a health alert signifying that everyone may experience more serious health effects."
+            }
+            else if (aqiIndex <= 500) {
+                bar.style.backgroundColor = "maroon";
+                y.innerHTML = "Conditions: Hazardous";
+                z.innerHTML = "This would trigger a health warnings of emergency conditions. The entire population is more likely to be affected."
+            }
+            else {
+                z.innerHTML = "We are unable to retrieve results at this time."
             }
         })
         .catch(
@@ -58,3 +98,20 @@ function showError(error) {
             break;
     }
 }
+
+function move(index) {
+  var elem = document.getElementById("myBar");   
+  var width = 1
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= index / 5) {
+      clearInterval(id);
+    } 
+    else {
+      width++; 
+      elem.style.width = width + '%';
+      elem.innerHTML = index;
+    }
+  }
+}
+
